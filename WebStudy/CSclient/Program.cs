333c7d79fee6c608ab529728f5c2fd98fa077b58
lan-lsgo <gui_052@163.com>
2018-01-03 21:24:27 +0800

    添加项目

---
+++ WebStudy/CSclient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CSclient
+{
+    /// <summary>
+    /// 套接字客户端代码
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建socket对象
+            Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+        }
+    }
+}
---
+++ WebStudy/IPHostEntry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace IPHostEntrySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length!=1)
+            {
+                Console.WriteLine("Usage:GetDNSHostInfo hostname");
+                return;
+            }
+
+            IPHostEntry result = Dns.GetHostByName(args[0]);
+            Console.WriteLine("Hostname：{0}", result.HostName);
+            //循环输出ip地址表
+            foreach(string alias in result.Aliases)
+            {
+                Console.WriteLine("Alias:{0}", alias);
+            }
+            //遍历地址列表
+            foreach(IPAddress address in result.AddressList)
+            {
+                Console.WriteLine("Address:{0}", address.ToString());
+            }
+        }
+    }
+}
