511d765339774eccd8072e450973b7faf22fb82e
lan-lsgo <gui_052@163.com>
2018-01-04 13:13:13 +0800

    CS模型

--- WebStudy/CSclient/Program.cs
+++ WebStudy/CSclient/Program.cs
@@ -17,7 +17,36 @@ namespace CSclient
         {
             //创建socket对象
             Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            //绑定IP地址
+            IPAddress adr = IPAddress.Parse("127.0.0.1");
+            //绑定端口
+            IPEndPoint ep = new IPEndPoint(adr, 3000);
+            //完成绑定
+            Server.Connect(ep);
 
+            int end = 0;
+            byte[] buffer=new byte[10];
+
+            //进行数据传输
+            if(Server.Receive(buffer) >0)
+            {
+                Console.WriteLine("连接上....");
+                Console.WriteLine("从服务器接收数据");
+
+                while(buffer[end]!=0)
+                {
+                    //显示缓冲区数据
+                    Console.WriteLine(buffer[end]);
+
+                    end++;
+                }
+
+                Console.WriteLine("断开连接");
+
+                //释放连接
+                Server.Shutdown(SocketShutdown.Both);
+                Server.Close();
+            }
         }
     }
 }
---
+++ WebStudy/CSserver/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CSserver
+{
+    /// <summary>
+    /// 套接字服务器代码
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //再笨地创建套接字，使用bind()方法将套接字与本地IP和端口号绑定
+
+            Socket client;
+            Socket ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            IPAddress adr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ep = new IPEndPoint(adr, 3000);
+
+            //类似客户端的connet()方法
+            ServerSocket.Bind(ep);
+            //开始监听
+            ServerSocket.Listen(3);
+
+            //开始传输数据
+            while(true)
+            {
+                if((client=ServerSocket.Accept())!=null)
+                {
+                    Console.WriteLine("连接上客户端，发送数据");
+                    byte[] message = { 10, 12, 30, 45, 20, 1, 0 };//结束标记为0
+                    //send()方法是返回发送的字节数
+                    Console.WriteLine("总计将发送" + client.Send(message) + "个字节数据");
+                    Console.WriteLine("发送完成");
+                    //关闭连接
+                    client.Close();
+                    break;
+                }
+            }
+        }
+    }
+}
