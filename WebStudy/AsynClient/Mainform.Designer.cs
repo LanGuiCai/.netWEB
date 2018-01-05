4aececb9480844f1c8d0401e0f4e22ce7c49e03c
lan-lsgo <gui_052@163.com>
2018-01-05 22:03:18 +0800

    新建窗体

---
+++ WebStudy/AsynClient/Mainform.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AsynClient
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbxInfo = new System.Windows.Forms.RichTextBox();
+            this.rtbxSend = new System.Windows.Forms.RichTextBox();
+            this.rtbxReceive = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "状态栏";
+            // 
+            // rtbxInfo
+            // 
+            this.rtbxInfo.Location = new System.Drawing.Point(23, 54);
+            this.rtbxInfo.Name = "rtbxInfo";
+            this.rtbxInfo.Size = new System.Drawing.Size(187, 96);
+            this.rtbxInfo.TabIndex = 1;
+            this.rtbxInfo.Text = " ";
+            // 
+            // rtbxSend
+            // 
+            this.rtbxSend.Location = new System.Drawing.Point(23, 206);
+            this.rtbxSend.Name = "rtbxSend";
+            this.rtbxSend.Size = new System.Drawing.Size(187, 119);
+            this.rtbxSend.TabIndex = 2;
+            this.rtbxSend.Text = "";
+            // 
+            // rtbxReceive
+            // 
+            this.rtbxReceive.Location = new System.Drawing.Point(278, 206);
+            this.rtbxReceive.Name = "rtbxReceive";
+            this.rtbxReceive.Size = new System.Drawing.Size(210, 118);
+            this.rtbxReceive.TabIndex = 3;
+            this.rtbxReceive.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(259, 79);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(66, 333);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "发送信息";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "接收信息";
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 368);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.rtbxReceive);
+            this.Controls.Add(this.rtbxSend);
+            this.Controls.Add(this.rtbxInfo);
+            this.Controls.Add(this.label1);
+            this.Name = "Mainform";
+            this.Text = "异步通信客户端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbxInfo;
+        private System.Windows.Forms.RichTextBox rtbxSend;
+        private System.Windows.Forms.RichTextBox rtbxReceive;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
